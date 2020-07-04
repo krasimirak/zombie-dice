8dc955d02ab8381b3fb10230abf49650b377797a
krasimirak <k_konstantinowa@hotmail.com>
2020-07-04 18:36:24 +0300

    Track used dice in the turn; Choose dices function

--- Sources/Turn/turn.swift
+++ Sources/Turn/turn.swift
@@ -5,16 +5,40 @@ public struct Turn {
     var currentFeet: Int = 0
     var currentBombs: Int = 0
     
-    mutating func chooseDice(numberOfDice: Int, diceCollection: [Die], inUse: [Bool]) {
-        var diceIndex: [Int] = []
-        for _ in 1...numberOfDice {
-            var die = Int.random(in: 0...12)
-            
-            while(inUse[die]) {
-                die = Int.random(in: 0...12)
+    var inUse: [Bool]
+    
+    init(diceCollectionCount: Int) {
+        for _ in 1...diceCollectionCount {
+            inUse.append(false)
+        }
+    }
+    
+    mutating func chooseDice(inout numberOfDice: Int, diceCollectionCount: Int, inout inUse: [Bool]) {
+        let diceIndexes = 0...(diceCollectionCount - 1)
+        
+        // check if needed dice for turn are more than the available dice
+        var freeDice: Int = 0
+        for i in diceIndexes {
+            if ! inUse[i] {
+                freeDice += 1
             }
+        }
+        
+        if freeDice < numberOfDice {
+            numberOfDice = freeDice // if there are less unused dice make the number of dice needed to be how many are available
+        }
+        
+        var resultDiceIndex: [Int] = []
+        var currentIndex: Int = 0
+        // get as many dice indexes as need checking if they are in use already
+        for _ in 1...numberOfDice {
             
-            diceIndex.append(die)
+            repeat {
+                currentIndex = Int.random(in: diceIndexes)
+            } while (inUse[currentIndex])
+            
+            resultDiceIndex.append(currentIndex)
+            inUse[currentIndex] = true
         }
     }
     
